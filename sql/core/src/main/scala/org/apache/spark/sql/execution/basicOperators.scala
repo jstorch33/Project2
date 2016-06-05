6ce26d3f94d35b02a54613d2a369725d7d88ce36
Jack Storch <jstorch33@gmail.com>
2016-06-05 14:26:40 -0700

    final

--- sql/core/src/main/scala/org/apache/spark/sql/execution/basicOperators.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/basicOperators.scala
@@ -103,7 +103,7 @@ case class PartitionProject(projectList: Seq[Expression], child: SparkPlan) exte
       var current_iterator: Iterator[Row] = null
 
       // Note: here we use the default number of partitions and default blockSize
-      val disk_partition_iterator = DiskHashedRelation(input, keyGenerator).getIterator()
+      val disk_partition_iterator = DiskHashedRelation(input, keyGenerator).getIterator
       var disk_partition: DiskPartition = null
       var cache_generator: (Iterator[Row] => Iterator[Row]) = null
 
